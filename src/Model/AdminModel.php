dbb391abd9dcebb853853a88469306a67b1f3b5e
Anddroid97 <litkovskiy97@gmail.com>
2019-05-25 20:44:42 +0300

    Add User Form

---
+++ src/Model/AdminModel.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Model;
+
+class AdminModel
+{
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * @var string $surname
+     */
+    private $surname;
+
+    /**
+     * @var string $email
+     */
+    private $email;
+
+    /**
+     * @var string $phone
+     */
+    private $phone;
+
+    /**
+     * @var string $role
+     */
+    private $role;
+
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+
+    /**
+     * @param string|null $name
+     * @return AdminModel
+     */
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param string|null $surname
+     * @return AdminModel
+     */
+    public function setSurname(?string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string|null $phone
+     * @return AdminModel
+     */
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+
+    /**
+     * @param $role
+     * @return AdminModel
+     */
+    public function setRole($role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+
+}
